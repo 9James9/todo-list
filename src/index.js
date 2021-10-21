53e308243a33e8d0de3621112e3f8c53c63ae508
James Driscoll <thejamesdriscoll@gmail.com>
2021-10-21 18:07:39 -0500

    removed old notes

--- src/index.js
+++ src/index.js
@@ -30,16 +30,7 @@ deleteListButton.addEventListener('click', e => {
     selectedListId = null
     saveAndRender()
 })
-/*
-//get values from the input forms
-function getValues() {
-    return {
-        title: document.querySelector('#todotitle').value,
-        description: document.querySelector('#tododescription').value,
-        priority: document.getElementById('priorityradio').elements["priority"].value
-    }
-}
-*/
+
 function render(){
     clearElement(listContainer)
     renderList()
@@ -125,23 +116,6 @@ function createList(name){
 
 }
 
-
-/*
-function newListInput(){
-    //creates form to collect the new list name, and a submit button
-   let chooseName = document.createElement('input')
-  let submitName = document.createElement('button')
-  submitName.textContent = "Submit"
-  document.getElementById('todobtns').appendChild(chooseName)
-  document.getElementById('todobtns').appendChild(submitName)
-
-  submitName.addEventListener('click', () => {
-      lists.push(chooseName.value)
-      renderList()
-  })
-}
-*/
-
 function save() {
     localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
     localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
@@ -156,45 +130,6 @@ function clearElement(element) {
         element.removeChild(element.firstChild)
     }
 }
-/*
-document.querySelector('#submit').addEventListener('click', addTodo)
-//document.querySelector('#addlist').addEventListener('click',newListInput)
-//create a div with the values from the input forms
-function addTodo() {
-    let data = getValues()
-    let mainContainer = document.querySelector('#todocontainer')
-    let newcontainer = document.createElement('div')
-    newcontainer.classList.add('todobox')
-    mainContainer.appendChild(newcontainer)
 
-    let todotitle = document.createElement('h2')
-    newcontainer.appendChild(todotitle)
-    todotitle.textContent = data.title
-
-    let tododescription = document.createElement('p')
-    newcontainer.appendChild(tododescription)
-    tododescription.textContent = data.description
-
-    let todopriority = document.createElement('p')
-    newcontainer.appendChild(todopriority)
-    todopriority.textContent = `Priority: ${data.priority}`
-
-    let removeBtn = document.createElement('button')
-    newcontainer.appendChild(removeBtn)
-    removeBtn.textContent = "Remove"
-    removeBtn.addEventListener('click', removeTodo)
-
-    clearValues()
-}
-*/
-/*
-function clearValues() {
-    document.querySelector('#todotitle').value = ""
-    document.querySelector('#tododescription').value = ""
-    document.getElementById('low').checked = false
-    document.getElementById('medium').checked = false
-    document.getElementById('high').checked = false
-}
-*/
 renderList()
 saveAndRender()
